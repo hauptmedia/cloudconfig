bad65351a4da4187f18265e1165233de4f64d594
Julian Haupt <jh@spielmeister.com>
2015-02-25 00:33:36 +0100

    removed wrong comments

--- www/features/etcd.php
+++ www/features/etcd.php
@@ -1,12 +1,4 @@
 <?php
-/*
- * ETCD_CA_FILE - location of CA certificate used to sign client certificates.
- * ETCD_CERT_FILE - location of ceritificate used for communication with clients
- * ETCD_KEY_FILE - location of private key used for communication with clients
- * ETCD_CA_FILE - location of CA certificate used for signing peer certificates. In my case it was the same CA.
- * ETCD_PEER_CERT_FILE - location of ceritificate used for communication with other etcd nodes
- * ETCD_PEER_KEY_FILE - location of private key used for communication with other etcd nodes
- */
 return function($clusterConfig, $nodeConfig, $cloudConfig) {
 
     if(!array_key_exists('coreos', $cloudConfig)) {
