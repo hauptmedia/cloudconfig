4763cf221daf02fd91f43d5806ec4fc6bd9a506a
Julian Haupt <jh@spielmeister.com>
2015-02-25 10:26:24 +0100

    changed project name in boot2docker script and added an option that allows you to override the IP setting

--- boot2docker.sh
+++ boot2docker.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
-#IP=$(/sbin/ifconfig eth1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
-IP="127.0.0.1:8080"
+if [ -z $IP ]; then
+    IP=$(/sbin/ifconfig eth1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
+fi
+
+if [ ! -d ./var ]; then
+    mkdir ./var
+fi
 
 dos2unix docker-entrypoint.sh
 
-docker build -t="hauptmedia/puppetmaster" .
+docker build -t="hauptmedia/cloudconfig" .
 
 docker run -i -t --rm \
 -p 80:80 \
--v $(pwd)/www:/var/www \
+-v $(pwd)/var:/opt/cloudconfig/var \
 -e BASE_URL=http://$IP \
-hauptmedia/puppetmaster \
+hauptmedia/cloudconfig \
 $@
