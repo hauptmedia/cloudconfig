2c9aeb152495c0679ca1b061661e94cc2aad182a
Julian Haupt <jh@spielmeister.com>
2015-02-26 15:37:42 +0100

    reformatted unit descriptions using inline code

--- features/ephemeral-drive.php
+++ features/ephemeral-drive.php
@@ -12,29 +12,29 @@ return function($clusterConfig, $nodeConfig, $cloudConfig) {
     $cloudConfig['coreos']['units'][] = array(
         'name'      => 'format-ephemeral.service',
         'command'   => 'start',
-        'content'   => '
-[Unit]
-Description=Formats the ephemeral drive
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-ExecStart=/usr/sbin/wipefs -f /dev/sdb
-ExecStart=/usr/sbin/mke2fs -q -t ext4 -b 4096 -i 4096 -I 128 /dev/sdb'
+        'content'   => 
+            "[Unit]\n" .
+            "Description=Formats the ephemeral drive\n" .
+            "[Service]\n" .
+            "Type=oneshot\n" .
+            "RemainAfterExit=yes\n" .
+            "ExecStart=/usr/sbin/wipefs -f /dev/sdb\n" .
+            "ExecStart=/usr/sbin/mke2fs -q -t ext4 -b 4096 -i 4096 -I 128 /dev/sdb'\n"
         );
 
     $cloudConfig['coreos']['units'][] = array(
         'name'      => 'var-lib-docker.mount',
         'command'   => 'start',
-        'content'   => '
-[Unit]
-Description=Mount ephemeral to /var/lib/docker
-Requires=format-ephemeral.service
-After=format-ephemeral.service
-Before=docker.service
-[Mount]
-What=/dev/sdb
-Where=/var/lib/docker
-Type=ext4'
+        'content'   => 
+            "[Unit]\n" .
+            "Description=Mount ephemeral to /var/lib/docker\n" .
+            "Requires=format-ephemeral.service\n" .
+            "After=format-ephemeral.service\n" .
+            "Before=docker.service\n" .
+            "[Mount]\n" .
+            "What=/dev/sdb\n" .
+            "Where=/var/lib/docker\n" .
+            "Type=ext4\n"
     );
 
             
