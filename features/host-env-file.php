f5e650f973a0cd555d41903664904bc4cb89b4e5
Julian Haupt <julian.haupt@hauptmedia.de>
2015-04-01 20:04:57 +0200

    removed outdated comment in host-env-file feature

--- features/host-env-file.php
+++ features/host-env-file.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * This feature writes the fleet metadata as an env file in /etc/host.env
- *
- * The env file can be used to pass the fleet metadata as environment variables in docker containers
- * with the --env-file=/etc/host.env docker command line option or in systemd service definitions
- * using the EnvironmentFile=/etc/host.env configuration option
- */
 return function($clusterConfig, $nodeConfig, $cloudConfig, $enabledFeatures) {
     //TODO: if no ip was specified via cloud config it must be set via a system service upon startup
     $public_ipv4 = "\$public_ipv4";
