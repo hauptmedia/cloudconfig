bdb589319df246412d7d110859c6435d62d46cee
Julian Haupt <julian.haupt@hauptmedia.de>
2015-04-01 20:12:43 +0200

    added HOSTNAME env variable to /etc/host.env

--- features/host-env-file.php
+++ features/host-env-file.php
@@ -1,17 +1,15 @@
 <?php
 return function($clusterConfig, $nodeConfig, $cloudConfig, $enabledFeatures) {
     //TODO: if no ip was specified via cloud config it must be set via a system service upon startup
-    $public_ipv4 = "\$public_ipv4";
-
-    if (array_key_exists('ip', $nodeConfig)) {
-        $public_ipv4 = $nodeConfig['ip'];
-    }
+    $public_ipv4    = array_key_exists('ip', $nodeConfig)       ? $nodeConfig['ip']         : "\$public_ipv4";
+    $hostname       = array_key_exists('hostname', $nodeConfig) ? $nodeConfig['hostname']   : "";
 
     if (!array_key_exists('write_files', $cloudConfig)) {
         $cloudConfig['write_files'] = array();
     }
 
-    $envFileContents = "PUBLIC_IPV4=" . $public_ipv4 . "\n";
+    $envFileContents  = "PUBLIC_IPV4=" . $public_ipv4 . "\n";
+    $envFileContents .= "HOSTNAME=" . $hostname . "\n";
 
     $cloudConfig['write_files'][] = array(
         'path'          => '/etc/host.env',
