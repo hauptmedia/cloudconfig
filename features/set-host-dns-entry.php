374bd7ab33633891ee7101c4e6e911b27ce724e0
Julian Haupt <julian.haupt@hauptmedia.de>
2015-04-02 15:24:49 +0200

    set the dns hostname 3 minutes after startup to ensure etcd is ready

--- features/set-host-dns-entry.php
+++ features/set-host-dns-entry.php
@@ -8,21 +8,32 @@ return function($clusterConfig, $nodeConfig, $cloudConfig, $enabledFeatures) {
     if(!array_key_exists('units', $cloudConfig['coreos'])) {
         $cloudConfig['coreos']['units'] = array();
     }
-    ///etc/host.env
+
     $cloudConfig['coreos']['units'][] = array(
         'name'      => 'set-host-dns-entry.service',
-        'command'   => 'start',
-        'content'   => 
+        'content'   =>
             "[Unit]\n" .
             "Description=Set the dns entry for this host\n" .
             "Wants=etcd.service\n" .
             "[Service]\n" .
-            "Type=oneshot\n" .
-            "RemainAfterExit=yes\n" .
+            "Type=simple\n" .
             "EnvironmentFile=/etc/host.env\n".
             "ExecStart=/opt/bin/skydns-set-record \${HOSTNAME} \${PUBLIC_IPV4}\n"
         );
-            
+
+    $cloudConfig['coreos']['units'][] = array(
+        'name'      => 'set-host-dns-entry.timer',
+        'command'   => 'start',
+        'content'   =>
+            "[Unit]\n" .
+            "Description=Set the dns entry for this host\n" .
+            "[Timer]\n" .
+            "OnBootSec=3min\n" .
+            "Unit=set-host-dns-entry.service\n".
+            "[Install]\n".
+            "WantedBy=multi-user.target"
+    );
+
     return $cloudConfig;
 
 };
