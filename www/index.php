5667e2255da262fcc493cd6577427b2eaf0982eb
Julian Haupt <julian.haupt@hauptmedia.de>
2015-04-28 18:53:42 +0200

    convert all errors to exceptions

--- www/index.php
+++ www/index.php
@@ -1,6 +1,12 @@
 <?php
 require_once('../vendor/autoload.php');
 
+function exception_error_handler($severity, $message, $file, $line) {
+	throw new ErrorException($message, 0, $severity, $file, $line);
+}
+
+set_error_handler("exception_error_handler");
+
 switch($_SERVER['REDIRECT_URL']) {
 	case '/install.sh':
 		require_once('install.sh.php');	
