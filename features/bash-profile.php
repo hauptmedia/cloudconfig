c498088e55e791516feee8e5a639371834e9bc86
Julian Haupt <julian.haupt@hauptmedia.de>
2015-04-28 20:42:33 +0200

    write /etc/etcdctl.env when etcd2 feature is enabled

--- features/bash-profile.php
+++ features/bash-profile.php
@@ -3,6 +3,9 @@ return function($clusterConfig, $nodeConfig) {
     $bashProfileContent  =
         "[[ -f ~/.bashrc ]] && . ~/.bashrc\n" .
         "\n" .
+        "if [ -f /etc/etcdctl.env ]; then\n".
+        "  . /etc/etcdctl.env\n" .
+        "fi\n" .
         "if [ -z \"\$SSH_AUTH_SOCK\" ] && [ -e /tmp/ssh-agent.sock ]; then\n" .
         "  export SSH_AUTH_SOCK=/tmp/ssh-agent.sock\n" .
         "fi\n";
--- features/etcd2.php
+++ features/etcd2.php
@@ -36,6 +36,19 @@ return function($clusterConfig, $nodeConfig) {
                     'command'   => 'start'
                 )
             )
+        ),
+
+        'write_files' => array(
+            array(
+                'owner'         => 'root:root',
+                'permissions'   => '0644',
+                'path'          => '/etc/etcdctl.env',
+                'content'       =>
+                    "ETCDCTL_PEERS=" . ( array_key_exists('etcd-peers', $clusterConfig) ? $clusterConfig['etcd-peers'] : "" ). "\n" .
+                    ($useSSL ? "ETCDCTL_CA_FILE=/etc/ssl/etcd/certs/ca.crt\n" : "") .
+                    ($useSSL ? "ETCDCTL_KEY_FILE=/etc/ssl/etcd/private/client.key\n" : "") .
+                    ($useSSL ? "ETCDCTL_CERT_FILE=/etc/ssl/etcd/certs/client.crt\n" : "")
+            )
         )
     );
 };
