6f53d4c9e27452d8ca026f010892e547ad066236
Julian Haupt <julian.haupt@hauptmedia.de>
2015-04-28 17:07:52 +0200

    moved etcd client certificate into it's own feature

--- features/etcd2-client-ssl.php
+++ features/etcd2-client-ssl.php
@@ -18,38 +18,19 @@ return function($clusterConfig, $nodeConfig, $cloudConfig, $enabledFeatures) {
 
     if (!empty($nodeConfig['etcd2']['name'])) {
         $etcdName = $nodeConfig['etcd2']['name'];
-        
+
     } elseif (!empty($nodeConfig['hostname'])) {
         $etcdName = $nodeConfig['hostname'];
     }
 
     $etcdCADir = realpath( __DIR__ . '/../var/etcd-ca' );
 
-    $serviceDefinition = 
-        "[Service]\n" .
-        "Environment=ETCD_PEER_CLIENT_CERT_AUTH=true\n" .
-        "Environment=ETCD_PEER_CA_FILE=/etc/ssl/etcd/certs/ca.crt\n" .
-        "Environment=ETCD_PEER_CERT_FILE=/etc/ssl/etcd/certs/peer.crt\n" .
-        "Environment=ETCD_PEER_KEY_FILE=/etc/ssl/etcd/private/peer.key\n" .
-        "Environment=ETCD_CLIENT_CERT_AUTH=true\n" .
-        "Environment=ETCD_CA_FILE=/etc/ssl/etcd/certs/ca.crt\n" .
-        "Environment=ETCD_CERT_FILE=/etc/ssl/etcd/certs/server.crt\n" .
-        "Environment=ETCD_KEY_FILE=/etc/ssl/etcd/private/server.key\n";
-
     $requiredFiles = array(
         $etcdCADir . '/certs/ca.crt'                             => '/etc/ssl/etcd/certs/ca.crt',
-        $etcdCADir . '/certs/' .      $etcdName . "-peer.crt"              => '/etc/ssl/etcd/certs/peer.crt',
-        $etcdCADir . '/private/' .    $etcdName . "-peer.key"              => '/etc/ssl/etcd/private/peer.key',
-        $etcdCADir . '/certs/' .      $etcdName . "-server.crt"              => '/etc/ssl/etcd/certs/server.crt',
-        $etcdCADir . '/private/' .    $etcdName . "-server.key"              => '/etc/ssl/etcd/private/server.key'
+        $etcdCADir . '/certs/' .      $etcdName . "-client.crt"              => '/etc/ssl/etcd/certs/client.crt',
+        $etcdCADir . '/private/' .    $etcdName . "-client.key"              => '/etc/ssl/etcd/private/client.key'
     );
-        
 
-    $cloudConfig['write_files'][] = array(
-        'path'          => '/run/systemd/system/etcd2.service.d/30-certificates.conf',
-        'permissions'   => '0644',
-        'content'       => $serviceDefinition
-    );
 
     foreach ($requiredFiles as $sourceFile => $destinationFile) {
         if (!file_exists($sourceFile)) {
