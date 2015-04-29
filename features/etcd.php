8ed0750472ba223f16729ad5cabba95692483f89
Julian Haupt <julian.haupt@hauptmedia.de>
2015-04-29 18:35:20 +0200

    readded etcd1 feature

---
+++ features/etcd.php
@@ -0,0 +1,30 @@
+<?php
+return function($clusterConfig, $nodeConfig) {
+    $etcdConfig    = $nodeConfig['etcd'];
+
+    if(!array_key_exists('name', $etcdConfig) && array_key_exists('hostname', $nodeConfig)) {
+        $etcdConfig['name'] = $nodeConfig['hostname'];
+    }
+
+    if(array_key_exists('ip', $nodeConfig)) {
+        if(!array_key_exists('addr', $etcdConfig)) {
+            $etcdConfig['addr'] = $nodeConfig['ip'] . ':2379';
+        }
+
+        if(!array_key_exists('peer-addr', $etcdConfig)) {
+            $etcdConfig['peer-addr'] = $nodeConfig['ip'] . ':2380';
+        }
+    }
+
+    return array(
+        'coreos' => array(
+            'etcd' => $etcdConfig,
+            'units' => array(
+                array(
+                    'name'      => 'etcd.service',
+                    'command'   => 'start'
+                )
+            )
+        )
+    );
+};
