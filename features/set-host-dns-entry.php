fcb76b27a4cd7760191d2aa6d281c733facff53e
Julian Haupt <julian.haupt@hauptmedia.de>
2015-04-29 11:18:33 +0200

    set-host-dns-entry is now an oneshot service

--- features/set-host-dns-entry.php
+++ features/set-host-dns-entry.php
@@ -8,20 +8,13 @@ return function($clusterConfig, $nodeConfig) {
                     'content'   =>
                         "[Unit]\n" .
                         "Description=Set the dns entry for this host\n" .
+                        "\n" .
                         "[Service]\n" .
-                        "Type=simple\n" .
+                        "Type=oneshot\n" .
+                        "RemainAfterExit=yes\n".
                         "EnvironmentFile=/etc/host.env\n".
-                        "ExecStart=/opt/bin/skydns-set-record \${HOSTNAME} \${PUBLIC_IPV4}\n"
-                ),
-                array(
-                    'name'      => 'set-host-dns-entry.timer',
-                    'command'   => 'start',
-                    'content'   =>
-                        "[Unit]\n" .
-                        "Description=Set the dns entry for this host\n" .
-                        "[Timer]\n" .
-                        "OnBootSec=3min\n" .
-                        "Unit=set-host-dns-entry.service\n".
+                        "ExecStart=/opt/bin/skydns-set-record \${HOSTNAME} \${PUBLIC_IPV4}\n".
+                        "\n" .
                         "[Install]\n".
                         "WantedBy=multi-user.target"
                 )
