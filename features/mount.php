cfe0b624c57b710cdb17170516626860b746f3a0
Julian Haupt <julian.haupt@hauptmedia.de>
2015-07-02 17:48:27 +0200

    removed format feature, merged into mount feature

--- features/mount.php
+++ features/mount.php
@@ -4,6 +4,18 @@ return function($clusterConfig, $nodeConfig) {
     $units = array();
 
     foreach($mountConfig as $mountConfigEntry) {
+        if(!array_key_exists('mount-point', $mountConfigEntry)) {
+            throw new \Exception("Missing mount-point in config");
+        }
+
+        if(!array_key_exists('dev', $mountConfigEntry)) {
+            throw new \Exception("Missing dev in config");
+        }
+
+        if(!array_key_exists('type', $mountConfigEntry) || $mountConfigEntry['type'] != 'ext4') {
+            throw new \Exception("Only type=ext4 supported in mount feature");
+        }
+
         $systemdName = substr(
             str_replace("/", "-", $mountConfigEntry["mount-point"]) . ".mount",
             1
@@ -20,6 +32,24 @@ return function($clusterConfig, $nodeConfig) {
                 "Where=" . $mountConfigEntry['mount-point'] . "\n" .
                 "Type=" . $mountConfigEntry['type'] . "\n"
         );
+
+
+        if(array_key_exists('format', $mountConfigEntry) && $mountConfigEntry['format']) {
+            $units[] = array(
+                'name'      => "format-" . substr(str_replace("/", "-", $mountConfigEntry["mount-point"]) . ".service", 1),
+                'command'   => 'start',
+                'content'   =>
+                    "[Unit]\n" .
+                    "Description=Formats a drive\n" .
+                    "Before=" . $systemdName . "\n" .
+                    "ConditionFirstBoot=yes\n" .
+                    "[Service]\n" .
+                    "Type=oneshot\n" .
+                    "RemainAfterExit=yes\n" .
+                    "ExecStart=/usr/sbin/wipefs -f " . $mountConfigEntry['dev'] . "\n" .
+                    "ExecStart=/usr/sbin/mke2fs -q -t ext4 -b 4096 -i 4096 -I 128 " . $mountConfigEntry['dev'] . "\n"
+            );
+        }
     }
 
     return array(
