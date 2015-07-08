e496f482a33c31278c8dc0f8d592b76590f9af93
Julian Haupt <julian.haupt@hauptmedia.de>
2015-07-08 19:13:49 +0200

    added new line

--- features/iptables.php
+++ features/iptables.php
@@ -35,6 +35,7 @@ return function($clusterConfig, $nodeConfig) {
 
     $iptablesRules .=
         // Log and drop everything else
+        "\n" .
         "-A Cloudconfig-Firewall-INPUT -j LOG\n" .
         "-A Cloudconfig-Firewall-INPUT -j REJECT --reject-with icmp-host-prohibited\n" .
         "COMMIT\n";
