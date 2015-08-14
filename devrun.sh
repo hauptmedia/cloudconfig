fa6396045512761e6832b3b48acc844413031f46
Julian Haupt <julian.haupt@hauptmedia.de>
2015-08-14 13:04:40 +0200

    moved boot2docker to devrun

--- devrun.sh
+++ devrun.sh
@@ -13,7 +13,7 @@ docker build -t="hauptmedia/cloudconfig" .
 
 docker run -i -t --rm \
 -p 8080:80 \
--v $(pwd)/../cloudconfig-settings:/opt/cloudconfig/var \
+-v $(pwd)/var:/opt/cloudconfig/var \
 -v $(pwd)/www:/opt/cloudconfig/www \
 -v $(pwd)/features:/opt/cloudconfig/features \
 -e BASE_URL=$BASE_URL \
